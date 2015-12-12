1492083d3395f52da754a12441da14e903a9444e
Alessandro Siletto <alessandro.siletto@gmail.com>
2015-12-12 20:15:12 +0100

    redis healthcheck

---
+++ service-common/src/main/java/eu/hcomb/common/healthcheck/RedisHealthCheck.java
@@ -0,0 +1,28 @@
+package eu.hcomb.common.healthcheck;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.google.inject.Inject;
+
+public class RedisHealthCheck extends HealthCheck {
+
+	@Inject
+	JedisPool pool;
+	
+	@Override
+	protected Result check() throws Exception {
+		try {
+			Jedis jedis = pool.getResource();
+			String pong = jedis.ping();
+            if ("PONG".equals(pong)) {
+                return Result.healthy();
+            }else{
+                return Result.unhealthy("Expecting PONG, got: " + pong);
+            }
+        }catch(Exception e){
+            return Result.unhealthy("Could not ping redis");
+        }
+	}
+}
